7156f0afe8f935f0edd6e595b72e213896dc6a15
Bill Caneira <wcaneira@travelers.com>
2022-09-13 13:34:55 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [fetchState, setFetchState] = useState({ fetching: true })
+
+  const handleRetry = () => {
+    setFetchState({ fetching: true })
+  }
+
+  useEffect(() => {
+    const doTheFetch = async () => {
+      try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        if (res.status === 200) {
+          const data = await res.json()
+          setFetchState({ todo: data.title, fetching: false })
+        }
+        else {
+          console.log('bad HTTP status', res.status)
+          setFetchState({ failed: true })
+        }
+      } catch (e) {
+        console.log('error thrown', e)
+        setFetchState({ failed: true })
+      }
+    }
+
+    if (fetchState.fetching) {
+      doTheFetch()
+    }
+  }, [fetchState])
+
+  if (fetchState.fetching) {
+    return <p>Loading...</p>
+  }
+
+  if (fetchState.failed) {
+    return <p>Failed! <button onClick={handleRetry}>Click here to try again</button></p>
+  }
+
+  return <p>{fetchState.todo}</p>
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+global.fetch = () => Promise.resolve({ status: 200, json: () => Promise.resolve({ title: 'testing123' })})
+
+test('renders loading indicator and then the fetched content, withOUT "act" warnings', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  //this version generates no "act" warning, neither does screen.queryByText
+  expect(screen.getByText('Loading...')).toBeInTheDocument()
+  
+  const text = await screen.findByText('testing123');
+  expect(text).toBeInTheDocument();
+});
+
+test('renders loading indicator and then the fetched content, WITH "act" warnings', async () => {
+  render(<App />);
+  
+  //...but this one SHOULD generate the warning, as soon as we wait; waitFor will cause the warning too
+  await screen.findByText('Loading...')
+
+  const text = await screen.findByText('testing123');
+  expect(text).toBeInTheDocument();
 });
